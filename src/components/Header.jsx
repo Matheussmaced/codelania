b5459eb5296cd13572df531ca0ad2da09a15edcf
Matheussmaced <mateusbyakuya3@gmail.com>
2023-08-01 21:05:32 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+
+const Header = () => {
+    return(
+     <>
+        <font/>
+            <header>
+                <div id="informationsHeader">
+                    <h1>Codelândia</h1>
+                    <h2>blog</h2>
+                </div>
+                <div id="barSearch">
+                    <input type="text" placeholder="Pesquisar no blog"/>
+                </div>
+            </header>
+    </>
+    )
+}
+
+export default Header
